3748979a7a8dd2624cfa9a832bfd8d8da184bb4d
Fatema Moharam <moharamfatema@gmail.com>
2022-11-12 23:04:16 +0200

    Squashed commit of the following:
    commit d4a34947d895547ef22e93807f9eef107c4ccde1
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Sat Nov 12 22:39:25 2022 +0200
    
        Added warping inverse to the wrap_prespective function
    
    commit 2cab6d34611deac5bf16ad2fdfc7def7d21ca3ce
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 22:51:51 2022 +0200
    
        Refactored compute_homography_ransac Exception message
    
    commit 69cb5146e64f01e43101421c5f16e44067d06648
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 22:44:28 2022 +0200
    
        Fixed a wrong variable reference in merge_two_images function
    
    commit b6354f14c56b7143d550de590863153577ff9d4b
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 22:42:38 2022 +0200
    
        Refactored get_image_sift_feature function
    
    commit 7ff9f20b5838ddb7bc4611718ade08a230741655
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 22:40:24 2022 +0200
    
        Refactored wrap_prespective
    
    commit 5454ea9def8da43782622e19e42370d577ead987
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 22:18:56 2022 +0200
    
        Refactored compute_homography_ransac function
    
    commit 658469eeac2df2194824d0822fdd12988b25fd69
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 22:14:51 2022 +0200
    
        Refactored get_matched_pt function
    
    commit 447f95fb1c7a8af8bea363c8f9d7160666e769eb
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 21:35:19 2022 +0200
    
        Refactored get_image_sift_feature function to check the dimentions of the passed image
    
    commit 7ec42b8f0b72fdb406c5166cf586702d3e49f87a
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 19:53:09 2022 +0200
    
        Added some markdown to image_mosaics.ipynb file
    
    commit 9d7fdf4766c33b9fd2c5e4fb3e365705607b47b2
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 19:49:23 2022 +0200
    
        Added example 3 in image_mosaics
    
    commit f514658d91031bebefc95b99c80cdfc75e952ada
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 19:46:10 2022 +0200
    
        Added example 2 in image_mosaics
    
    commit 0256b0608a9c22394866ea4ea4780643307f4776
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 19:32:50 2022 +0200
    
        Added example 1 in image_mosaics
    
    commit 03125ec14c412f6d885ded324ac036d349e79d0e
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 18:30:39 2022 +0200
    
        Implemented crop_img_black_parts_horizontaly function
    
    commit 57241fe9a0ff47adfe003795f159b78ed9e0ff92
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 17:26:55 2022 +0200
    
        Implemented wrap_prespective function in common.py
    
    commit 4340be56145c4a24bc7bc263755c9fddf2761eb0
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 17:25:59 2022 +0200
    
        Implemented show_image function in common.py
    
    commit 27b48838b8a3db7e319d403cbe430ef7d62db9a0
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 17:24:46 2022 +0200
    
        Implemented compute_homography_ransac with its helper functions
    
    commit d4d5c305b17297c9e216ca6f173d600ae8d377b5
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 17:23:49 2022 +0200
    
        Refactored get_homograph_mat function in common.py
    
    commit 8fda5ceed3eadd92e81797d6146329dc082cf020
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 17:22:48 2022 +0200
    
        Refactored get_matches function in common.py
    
    commit d2fe25bd2409cc2ae9377c606ce51d7e81b23cd9
    Author: MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
    Date:   Fri Nov 11 17:21:42 2022 +0200
    
        Added image_mosaics.ipynb file

--- common.py
+++ common.py
@@ -1,58 +1,184 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+import math
+
+# RANSAC parameters
+SAMPLE_SIZE = 5 #number of point correspondances for estimation of Homgraphy
+SUCCESS_PROB = 0.995 #required probabilty of finding H with all samples being inliners
 
 # 1.1 Getting Correspondences
 # Feature of the two image using SIFT
-
-
 def get_image_sift_feature(img):
-    assert img.ndim == 2, "Image should be gray scale"
+    # Check if the image need to be converted to gray
+    gray_img = img
+    if img.ndim != 2:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     # create SIFT object
     sift = cv2.SIFT_create()
     # detect SIFT features in both images
-    keypoints, descriptors = sift.detectAndCompute(img, None)
+    keypoints, descriptors = sift.detectAndCompute(gray_img, None)
+
     return keypoints, descriptors
 
+
 # Get the matched feature between the two image
-
-
-def get_matches(des1, des2, ratio=0.5, knn_num=2):
+def get_matches(des1, des2, ratio=0.75):
     # Brute force matcher
     bf = cv2.BFMatcher()
     # match descriptors of both images
-    matches = bf.knnMatch(des1, des2, k=knn_num)
+    matches = bf.knnMatch(des1, des2, k=2)
     matches_list = []
     for m, n in matches:
         if m.distance < ratio * n.distance:
             matches_list.append([m])
-    return matches_list
+
+    return sorted(matches_list,key=lambda x:x[0].distance) # sort results from the best keypoints to worst
 
 # 1.2 Compute the Homography Parameters
 # The point that match the 2 image
+def get_matched_pt(kpt, matched_list, points_type = 0, num_pts=50):
+    # TODO: check if the function returning the desirable array shape
+    """Function to extract the (x, y) points from brute-force knn matcher points
 
-def get_matched_pt(kpt, matched_list, num_pts=50, query=True):
-    pts = np.float32(
-        [kpt[m[0].queryIdx].pt if query else kpt[m[0].trainIdx].pt for m in matched_list[:num_pts]]).reshape(-1, 1, 2)
+    Args:
+        kpt (cv2.KeyPoint array): of shape (n, 1)
+        matched_list (2D cv2.DMatch array): of shape (n, 1, 1) represting n set of x, y points
+        points_type (number): pass 0 for query points, and 1 for train points
+    """
+    pts = np.float32([kpt[m[0].queryIdx if points_type == 0 else m[0].trainIdx].pt for m in matched_list[:num_pts]])
+
     return pts
 
 # Get the homograph matrix using the SVD
-def get_homograph_mat(pts1, pts2):
-    a_mat = np.zeros((pts1.shape[0] * 2, 9))
-    ind = 0
+def get_homograph_mat(pts_dst, pts_src):
+    a_mat = np.zeros((pts_src.shape[0] * 2, 9))
+
     # Build the A matrix Ah=0
-    for i, (pt1, pt2) in enumerate(zip(pts1, pts2)):
-        x = pt1[0][0]
-        y = pt1[0][1]
-        xd = pt2[0][0]
-        yd = pt2[0][1]
-        x_xd = x * xd
-        y_xd = y * xd
-        x_yd = x * yd
-        y_yd = y * yd
-        a_mat[ind][0], a_mat[ind][1], a_mat[ind][2], a_mat[ind][6], a_mat[ind][7], a_mat[ind][8] = -x, -y, -1, x_xd, y_xd, xd
-        a_mat[ind+1][3], a_mat[ind+1][4], a_mat[ind+1][5], a_mat[ind +1][6], a_mat[ind+1][7], a_mat[ind+1][8] = -x, -y, -1, x_yd, y_yd, yd
-        ind += 2
+    for i in range(len(pts_src)):
+        x = pts_src[i][0]
+        y = pts_src[i][1]
+        x_dash = pts_dst[i][0]
+        y_dash = pts_dst[i][1]
+        a_mat[i * 2] += [-x , -y, -1, 0, 0, 0, x * x_dash, y * x_dash, x_dash]
+        a_mat[i * 2 + 1] += [0, 0, 0, -x, -y, -1, x * y_dash, y * y_dash, y_dash]
+
     U, D, V = np.linalg.svd(a_mat, full_matrices=False)
     # Smallest singular value
     homography_mat = (V[-1] / V[-1][-1]).reshape((3, 3))
+
     return homography_mat
+
+
+def transform_with_homography(h_mat, points_array):
+    # add column of ones so that matrix multiplication with homography matrix is possible
+    ones_col = np.ones((points_array.shape[0], 1))
+    points_array = np.concatenate((points_array, ones_col), axis=1)
+    transformed_points = np.matmul(h_mat, points_array.T)
+    epsilon = 1e-7 # very small value to use it during normalization to avoid division by zero
+    transformed_points = transformed_points / (transformed_points[2,:].reshape(1,-1) + epsilon)
+    transformed_points = transformed_points[0:2,:].T
+
+    return transformed_points
+
+def compute_outliers(h_mat, points_img_a, points_img_b, threshold):
+    outliers_count = 0
+
+    # transform the match point in image B to image A using the homography
+    points_img_b_hat = transform_with_homography(h_mat, points_img_b)
+
+    # let x, y be coordinate representation of points in image A
+    # let x_hat, y_hat be the coordinate representation of transformed points of image B with respect to image A
+    x = points_img_a[:, 0]
+    y = points_img_a[:, 1]
+    x_hat = points_img_b_hat[:, 0]
+    y_hat = points_img_b_hat[:, 1]
+    euclid_dis = np.sqrt(np.power((x_hat - x), 2) + np.power((y_hat - y), 2)).reshape(-1)
+    for dis in euclid_dis:
+        if dis > threshold:
+            outliers_count += 1
+
+    return outliers_count
+
+
+def compute_homography_ransac(pts_dst, pts_src, CONFIDENCE_THRESH = 65, OUTLIER_DIS_THRESH = 3):
+    num_all_matches =  pts_dst.shape[0]
+    min_iterations = int(np.log(1.0 - SUCCESS_PROB)/np.log(1 - 0.5**SAMPLE_SIZE))
+
+    # Let the initial error be large i.e consider all matched points as outliers
+    lowest_outliers_count = num_all_matches
+    best_h_mat = None
+
+    for i in range(min_iterations):
+        rand_ind = np.random.permutation(range(num_all_matches))[:SAMPLE_SIZE]
+        h_mat = get_homograph_mat(pts_dst[rand_ind], pts_src[rand_ind])
+        outliers_count = compute_outliers(h_mat, pts_dst, pts_src, OUTLIER_DIS_THRESH)
+        if outliers_count < lowest_outliers_count:
+            best_h_mat = h_mat
+            lowest_outliers_count = outliers_count
+
+    best_confidence_obtained = int(100 - (100 * lowest_outliers_count / num_all_matches))
+    if best_confidence_obtained < CONFIDENCE_THRESH:
+        raise Exception(f'The obtained confidence ratio was {best_confidence_obtained}% which is not higher than {CONFIDENCE_THRESH}%')
+
+    return best_h_mat
+
+
+def show_image(img, x_axes_visible = False, y_axes_visible = False):
+  ax = None
+  if len(img.shape) == 3:
+    ax = plt.imshow(img[:,:,::-1])
+  else:
+    ax = plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+
+  ax.axes.get_xaxis().set_visible(x_axes_visible)
+  ax.axes.get_yaxis().set_visible(y_axes_visible)
+  plt.show()
+
+
+def point_is_out_of_range(point, dim):
+    return point[0] < 0 or point[0] >= dim[0] or point[1] < 0 or point[1] >= dim[1]
+
+def wrap_prespective(img, h, dim):
+    target_img = np.zeros((dim[1], dim[0], 3), dtype=np.float64)
+    count_mat = np.zeros((dim[1], dim[0]), dtype=np.int32)
+    for y in range(len(img)):
+        for x in range(len(img[y])):
+            curr_coord = [[x], [y], [1]]
+            new_coord = np.dot(h, curr_coord)
+            new_coord[0][0] /= new_coord[2][0]
+            new_coord[1][0] /= new_coord[2][0]
+            new_x_points = [int(math.floor(new_coord[0][0])), int(math.ceil(new_coord[0][0]))]
+            new_y_points = [int(math.floor(new_coord[1][0])), int(math.ceil(new_coord[1][0]))]
+            for new_x in new_x_points:
+                for new_y in new_y_points:
+                    if not point_is_out_of_range((new_x, new_y), dim):
+                        target_img[new_y, new_x, :] += img[y, x, :]
+                        count_mat[new_y, new_x] += 1
+
+
+    h_inv = np.linalg.inv(h)
+    for y in range(len(target_img)):
+        for x in range(len(target_img[y])):
+            if count_mat[y, x] == 0:
+                curr_coord = [[x], [y], [1]]
+                new_coord = np.dot(h_inv, curr_coord)
+                new_coord[0][0] /= new_coord[2][0]
+                new_coord[1][0] /= new_coord[2][0]
+                new_x_points = [int(math.floor(new_coord[0][0])), int(math.ceil(new_coord[0][0]))]
+                new_y_points = [int(math.floor(new_coord[1][0])), int(math.ceil(new_coord[1][0]))]
+                weighted_intenisty_sum = np.array([0, 0, 0], dtype=np.float64)
+                for new_x in new_x_points:
+                    for new_y in new_y_points:
+                        if not point_is_out_of_range((new_x, new_y), (img.shape[1], img.shape[0])):
+                            weighted_intenisty_sum += abs(new_x - new_coord[0][0]) * abs(new_y - new_coord[1][0]) * img[new_y, new_x]
+
+                target_img[y, x] += weighted_intenisty_sum / 4
+
+            else:
+                target_img[y, x, 0] = int(np.round(target_img[y, x, 0] / count_mat[y, x]))
+                target_img[y, x, 1] = int(np.round(target_img[y, x, 1] / count_mat[y, x]))
+                target_img[y, x, 2] = int(np.round(target_img[y, x, 2] / count_mat[y, x]))
+
+    return target_img.astype(np.uint8)
